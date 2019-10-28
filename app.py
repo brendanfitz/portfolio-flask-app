ad868fbd522159b7af260b606d77b42423c6f2d6
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2019-10-28 09:17:21 -0400

    Update route names with project numbers; Change file paths for images to url_for

--- app.py
+++ app.py
@@ -12,23 +12,23 @@ def about():
 
 @app.route('/benson')
 def benson():
-    return render_template('benson.html')
+    return render_template('01-benson.html')
 
 @app.route('/luther')
 def luther():
-    return render_template('luther.html')
+    return render_template('02-luther.html')
 
 @app.route('/mcnulty')
 def mcnulty():
-    return render_template('mcnulty.html')
+    return render_template('03-mcnulty.html')
 
 @app.route('/fletcher')
 def fletcher():
-    return render_template('fletcher.html')
+    return render_template('04-fletcher.html')
 
 @app.route('/kojak')
 def kojak():
-    return render_template('kojak.html')
+    return render_template('05-kojak.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
