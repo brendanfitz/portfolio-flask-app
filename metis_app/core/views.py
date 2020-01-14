ce967e5f2b25054fe08a917e7ed50174c46a8984
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-01-13 19:06:43 -0500

    Redo index page

--- metis_app/core/views.py
+++ metis_app/core/views.py
@@ -1,11 +1,11 @@
 # core/views.py
 from flask import render_template, request, Blueprint
 
-core = Blueprint('core', __name__)
+core = Blueprint('core', __name__, template_folder='templates/core')
 
 @core.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', homepage=True)
 
 @core.route('/about')
 def about():
