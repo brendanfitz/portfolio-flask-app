bc7b2348bf8e33383eb0c02afa7c73f2d4abb442
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-01-22 21:53:11 -0500

    Readd visuals back in

--- metis_app/visuals/db.py
+++ metis_app/visuals/db.py
@@ -1,29 +1,4 @@
 blog_db = {
-    'benson': {
-        'title': 'Data Cleaning and Daily Commuters',
-        'subtitle': 'Exploratory Analysis of New York Subway Traffic',
-        'template': '01-benson.html',
-    },
-    'luther': {
-        'title': 'Forecasting Films',
-        'subtitle': 'Predicting Opening Weekend Success',
-        'template': '02-luther.html',
-        'template_visuals': 'genre_chart.html',
-    },
-    'mcnulty': {
-        'title': 'Cracking the Club\'s Code',
-        'subtitle': 'Predicting LendingClub Default Rates',
-        'template': '03-mcnulty.html',
-        'template_visuals': 'lending_club_chloropleth.html',
-    },
-    'fletcher': {
-        'title': 'Pitch Perfecting',
-        'subtitle': 'Unsupervised Learning of Kickstarter Data',
-        'template': '04-fletcher.html',
-    },
-    'kojak': {
-        'title': 'Magic Through Machine Learning',
-        'subtitle': 'Recommending Hogwarts Houses using NLP Analysis',
-        'template': '05-kojak.html',
-    },
+    'genre_chart': 'genre_chart.html',
+    'chloropleth': 'lending_club_chloropleth.html',
 }
--- metis_app/visuals/views.py
+++ metis_app/visuals/views.py
@@ -2,10 +2,12 @@
 from flask import render_template, request, Blueprint
 from metis_app.visuals.db import blog_db
 
-visuals = Blueprint('visuals', __name__)
+kwargs = dict(
+    template_folder='templates/visuals',
+)
+visuals = Blueprint('visuals', __name__, **kwargs)
 
-@visuals.route('/visuals/<name>')
-def visuals(name):
-    blog_data = blog_db[name]
-    template = '/visuals/{}'.format(blog_data['template_visuals']) # MUST UPDATE THIS
-    return render_template(template)
+@visuals.route('/<name>')
+def visual(name):
+    template = blog_db[name]
+    return render_template(template, viz_page=True)
