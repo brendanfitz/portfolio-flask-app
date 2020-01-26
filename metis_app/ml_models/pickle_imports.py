b808fe616ac6edfe7f89311a391d95116776bd44
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-01-26 17:41:05 -0500

    Reinsert models blueprint into refactor

--- metis_app/ml_models/pickle_imports.py
+++ metis_app/ml_models/pickle_imports.py
@@ -1,36 +1,42 @@
 import pickle
-import pickle
 from sklearn.preprocessing import PolynomialFeatures, OneHotEncoder, StandardScaler
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LinearRegression, Lasso, LassoCV
 import numpy as np
 import pandas as pd
-from metis_app.models import mcnulty_util
+from metis_app.ml_models import luther_util
+from metis_app.ml_models import mcnulty_util
 
-filename = 'templates/models/pickles/luther_model.pkl'
-with open(filename, 'rb') as f:
-    regr = pickle.load(f)
+import sys
+sys.path.append('metis_app/ml_models')
 
-filename = 'templates/models/pickles/budget_poly.pkl'
-with open(filename, 'rb') as f:
-    budget_poly = pickle.load(f)
+class Pickle_Imports:
 
-filename = 'templates/models/pickles/budget_poly_scaler.pkl'
-with open(filename, 'rb') as f:
-    budget_poly_scaler = pickle.load(f)
+    def __init__(self):
+        filename = 'metis_app/static/pickles/luther_model.pkl'
+        with open(filename, 'rb') as f:
+            self.regr = pickle.load(f)
 
-filename = 'templates/models/pickles/ohe.pkl'
-with open(filename, 'rb') as f:
-    ohe = pickle.load(f)
+        filename = 'metis_app/static/pickles/budget_poly.pkl'
+        with open(filename, 'rb') as f:
+            self.budget_poly = pickle.load(f)
 
-filename = 'templates/models/pickles/cv.pkl'
-with open(filename, 'rb') as f:
-    cv = pickle.load(f)
+        filename = 'metis_app/static/pickles/budget_poly_scaler.pkl'
+        with open(filename, 'rb') as f:
+            self.budget_poly_scaler = pickle.load(f)
 
-filename = 'templates/models/pickles/passthroughs_scaler.pkl'
-with open(filename, 'rb') as f:
-    passthroughs_scaler = pickle.load(f)
+        filename = 'metis_app/static/pickles/ohe.pkl'
+        with open(filename, 'rb') as f:
+            self.ohe = pickle.load(f)
 
-filename = 'templates/models/pickles/clf.pkl'
-with open(filename, 'rb') as f:
-    clf = pickle.load(f)
+        filename = 'metis_app/static/pickles/cv.pkl'
+        with open(filename, 'rb') as f:
+            self.cv = pickle.load(f)
+
+        filename = 'metis_app/static/pickles/passthroughs_scaler.pkl'
+        with open(filename, 'rb') as f:
+            self.passthroughs_scaler = pickle.load(f)
+
+        filename = 'metis_app/static/pickles/clf.pkl'
+        with open(filename, 'rb') as f:
+            self.clf = pickle.load(f)
--- metis_app/ml_models/views.py
+++ metis_app/ml_models/views.py
@@ -1,47 +1,63 @@
 # ml_models/views.py
+import numpy as np
+import pandas as pd
 from flask import render_template, request, Blueprint
 from metis_app.ml_models.forms import MoviePredictorForm, LoanPredictorForm
-from metis_app.ml_models.db import blog_db
+from metis_app.ml_models.pickle_imports import Pickle_Imports
 
-ml_models = Blueprint('ml_models', __name__)
+
+ml_models = Blueprint('ml_models', __name__, template_folder="templates/ml_models")
+
+pickles = Pickle_Imports()
+
+def luther_prediction(form):
+    budget_df = pickles.budget_poly_scaler.transform(pickles.budget_poly.transform([[form.budget.data]]))
+    passthroughs_df = pickles.passthroughs_scaler.transform([
+       [form.in_release_days.data, form.widest_release.data, form.runtime.data],
+    ])
+    rating_df = pickles.ohe.transform([[form.rating.data]]).toarray()
+    genre_df = pickles.cv.transform([form.genre.data]).toarray()
+    frames = [budget_df, passthroughs_df, rating_df, genre_df]
+    row = np.concatenate(frames, axis=1)
+    prediction = pickles.regr.predict(row)[0]
+    return prediction
+
+def mcnulty_prediction(form):
+    row = pd.DataFrame({
+        'dti': form.dti.data,
+        'int_rate': form.int_rate.data,
+        'emp_length': form.emp_length.data,
+        'home_ownership': form.home_ownership.data,
+        'purpose': form.purpose.data,
+        'delinq_2yrs': form.delinq_2yrs.data,
+        'revol_bal': form.revol_bal.data,
+        'loan_amnt': form.loan_amnt.data,
+        'grade': form.grade.data,
+        'term': form.term.data,
+        'installment': form.installment.data,
+        'addr_state': form.addr_state.data,
+    }, index=[0])
+    prediction = "{:0.1%}".format(pickles.clf.predict_proba(row)[0][1])
+    return prediction
 
 @ml_models.route('/<name>', methods=['GET', 'POST'])
 def models(name):
-    blog_data = blog_db[name]
-    template = '/models/{}'.format(blog_data['template'])
+    template = '{}.html'.format(name)
+
     if name == 'luther':
         form = MoviePredictorForm()
+        title = "Movie ROI Prediction Model"
     elif name == 'mcnulty':
         form = LoanPredictorForm()
+        title = "Lending Club Loan Default Prediction Model"
     else:
         form = None
+
     if request.method == 'POST':
         if name == 'luther':
-            budget_df = budget_poly_scaler.transform(budget_poly.transform([[form.budget.data]]))
-            passthroughs_df = passthroughs_scaler.transform([
-               [form.in_release_days.data, form.widest_release.data, form.runtime.data],
-            ])
-            rating_df = ohe.transform([[form.rating.data]]).toarray()
-            genre_df = cv.transform([form.genre.data]).toarray()
-            frames = [budget_df, passthroughs_df, rating_df, genre_df]
-            row = np.concatenate(frames, axis=1)
-            prediction = regr.predict(row)[0]
+            prediction = luther_prediction(form)
         elif name == 'mcnulty':
-            print(type(form.dti.data))
-            row = pd.DataFrame({
-                'dti': form.dti.data,
-                'int_rate': form.int_rate.data,
-                'emp_length': form.emp_length.data,
-                'home_ownership': form.home_ownership.data,
-                'purpose': form.purpose.data,
-                'delinq_2yrs': form.delinq_2yrs.data,
-                'revol_bal': form.revol_bal.data,
-                'loan_amnt': form.loan_amnt.data,
-                'grade': form.grade.data,
-                'term': form.term.data,
-                'installment': form.installment.data,
-                'addr_state': form.addr_state.data,
-            }, index=[0])
-            prediction = "{:0.1%}".format(clf.predict_proba(row)[0][1])
-        return render_template(template, form=form, prediction=prediction)
-    return render_template(template, form=form)
+            prediction = mcnulty_prediction(form)
+        return render_template(template, model=True, form=form, title=title, prediction=prediction)
+
+    return render_template(template, model=True, title=title, form=form)
