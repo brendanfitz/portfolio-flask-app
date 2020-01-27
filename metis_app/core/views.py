d415f405532b990d53f7fd4e3ab66e0b376cf394
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-01-27 18:14:26 -0500

    Add blog_db to core views

--- metis_app/core/views.py
+++ metis_app/core/views.py
@@ -1,11 +1,12 @@
 # core/views.py
 from flask import render_template, request, Blueprint
+from metis_app.blog_posts.db import blog_db
 
 core = Blueprint('core', __name__, template_folder='templates/core')
 
 @core.route('/')
 def index():
-    return render_template('index.html', homepage=True)
+    return render_template('index.html', homepage=True, blog_db=blog_db)
 
 @core.route('/about')
 def about():
