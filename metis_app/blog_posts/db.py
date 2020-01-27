19af0cc6c42f8826bc1d38938b395cd3b35e06ca
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-01-26 19:23:25 -0500

    Remove metis_blog_db

--- metis_app/blog_posts/db.py
+++ metis_app/blog_posts/db.py
@@ -11,35 +11,7 @@ blog_db = {
     'movie_data_eda': {
         'title': 'Box Office Mojo Top 5 EDA Fun Facts',
     },
-}
-
-
-metis_blog_db = {
-    'benson': {
-        'title': 'Data Cleaning and Daily Commuters',
-        'subtitle': 'Exploratory Analysis of New York Subway Traffic',
-        'template': '01-benson.html',
-    },
-    'luther': {
-        'title': 'Forecasting Films',
-        'subtitle': 'Predicting Opening Weekend Success',
-        'template': '02-luther.html',
-        'template_visuals': 'genre_chart.html',
-    },
-    'mcnulty': {
-        'title': 'Cracking the Club\'s Code',
-        'subtitle': 'Predicting LendingClub Default Rates',
-        'template': '03-mcnulty.html',
-        'template_visuals': 'lending_club_chloropleth.html',
-    },
-    'fletcher': {
-        'title': 'Pitch Perfecting',
-        'subtitle': 'Unsupervised Learning of Kickstarter Data',
-        'template': '04-fletcher.html',
-    },
-    'kojak': {
-        'title': 'Magic Through Machine Learning',
-        'subtitle': 'Recommending Hogwarts Houses using NLP Analysis',
-        'template': '05-kojak.html',
+    'redshift': {
+        'title': 'Redshift SQL Helper',
     },
 }
