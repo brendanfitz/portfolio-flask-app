418a62532dfdcbcf459984c2a6d693ecd7dacca9
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-02-10 08:56:13 -0500

    Move choices lists to csv in ml_model forms

--- metis_app/ml_models/forms.py
+++ metis_app/ml_models/forms.py
@@ -3,6 +3,20 @@ from wtforms import (
     StringField, IntegerField, SelectField, FloatField, SubmitField,
     TextAreaField, validators
 )
+import os
+import pandas as pd
+
+def choices_list(filename):
+    filepath = os.path.join(
+        'metis_app',
+        'ml_models',
+        'static',
+        filename,
+    )
+    return (pd.read_csv(filepath, index_col=0)
+        .to_records()
+        .tolist()
+    )
 
 class KickstarterPitchOutcomeForm(FlaskForm):
     pitch = TextAreaField('Pitch')
@@ -20,32 +34,8 @@ class MoviePredictorForm(FlaskForm):
     )
     genre = SelectField(
         'Genre',
-        choices=[
-            ("action", "Action"),
-            ("adventure", "Adventure"),
-            ("animation", "Animation"),
-            ("comedy", "Comedy"),
-            ("concert", "Concert"),
-            ("crime", "Crime"),
-            ("documentary", "Documentary"),
-            ("drama", "Drama"),
-            ("epic", "Epic"),
-            ("family", "Family"),
-            ("fantasy", "Fantasy"),
-            ("historical", "Historical"),
-            ("horror", "Horror"),
-            ("music", "Music"),
-            ("musical", "Musical"),
-            ("period", "Period"),
-            ("romance", "Romance"),
-            ("romantic", "Romantic"),
-            ("sci-fi", "Sci-Fi"),
-            ("sports", "Sports"),
-            ("thriller", "Thriller"),
-            ("war", "War"),
-            ("western", "Western"),
-        ],
-        default='action'
+        choices=choices_list('genre_choices.csv'),
+        default='action',
     )
     submit = SubmitField("Predict")
 
@@ -54,51 +44,17 @@ class LoanPredictorForm(FlaskForm):
     int_rate = FloatField('Interest Rate (%)', default=13.8)
     emp_length = SelectField(
         'Employment Length',
-        choices=[
-            ('< 1 year', '< 1 year'),
-            ('1 year', '1 year'),
-            ('2 years', '2 years'),
-            ('3 years', '3 years'),
-            ('4 years', '4 years'),
-            ('5 years', '5 years'),
-            ('6 years', '6 years'),
-            ('7 years', '7 years'),
-            ('8 years', '8 years'),
-            ('9 years', '9 years'),
-            ('10+ years', '10+ years'),
-            ('Not provided', 'Not provided'),
-        ],
+        choices=choices_list('emp_length_choices.csv'),
         default='10+ years',
     )
     home_ownership = SelectField(
         'Home Ownership Status',
-        choices=[
-            ('MORTGAGE', 'Mortage'),
-            ('OWN', 'Own'),
-            ('RENT', 'Rent'),
-            ('OTHER', 'Other'),
-            ('NONE', 'None'),
-        ],
+        choices=choices_list('home_ownership_choices.csv'),
         default='Mortgage',
     )
     purpose = SelectField(
         'Purpose of Loan',
-        choices=[
-            ('car', 'Car'),
-            ('credit_card', 'Credit Card'),
-            ('debt_consolidation', 'Debt Consolidation'),
-            ('educational', 'Educational'),
-            ('home_improvement', 'Home Improvement'),
-            ('house', 'House'),
-            ('major_purchase', 'Major Purchase'),
-            ('medical', 'Medical'),
-            ('moving', 'Moving'),
-            ('renewable_energy', 'Renewable Energy'),
-            ('small_business', 'Small Business'),
-            ('vacation', 'Vacation'),
-            ('wedding', 'Wedding'),
-            ('other', 'Other'),
-        ],
+        choices=choices_list('purpose_choices.csv'),
         default='Debt Consolidation',
     )
     delinq_2yrs = SelectField(
@@ -110,81 +66,18 @@ class LoanPredictorForm(FlaskForm):
     loan_amnt = FloatField('Loan Amount', default=13658)
     grade = SelectField(
         'Lending Club Loan Grade',
-        choices=[
-            ('A', 'A'),
-            ('B', 'B'),
-            ('C', 'C'),
-            ('D', 'D'),
-            ('E', 'E'),
-            ('F', 'F'),
-            ('G', 'G'),
-        ],
+        choices=choices_list('grade_choices.csv'),
         default='B',
     )
     term = SelectField(
         'Loan Term',
-        choices=[
-            ('36 months', '36 months'),
-            ('60 months', '60 months'),
-        ],
+        choices=[('36 months', '36 months'), ('60 months', '60 months'),],
         default='36 months',
     )
     installment = FloatField('Installment', default=420)
     addr_state = SelectField(
         'State',
-        choices=[
-            ('AK', 'AK'),
-            ('AL', 'AL'),
-            ('AR', 'AR'),
-            ('AZ', 'AZ'),
-            ('CA', 'CA'),
-            ('CO', 'CO'),
-            ('CT', 'CT'),
-            ('DC', 'DC'),
-            ('DE', 'DE'),
-            ('FL', 'FL'),
-            ('GA', 'GA'),
-            ('HI', 'HI'),
-            ('IA', 'IA'),
-            ('ID', 'ID'),
-            ('IL', 'IL'),
-            ('IN', 'IN'),
-            ('KS', 'KS'),
-            ('KY', 'KY'),
-            ('LA', 'LA'),
-            ('MA', 'MA'),
-            ('MD', 'MD'),
-            ('ME', 'ME'),
-            ('MI', 'MI'),
-            ('MN', 'MN'),
-            ('MO', 'MO'),
-            ('MS', 'MS'),
-            ('MT', 'MT'),
-            ('NC', 'NC'),
-            ('ND', 'ND'),
-            ('NE', 'NE'),
-            ('NH', 'NH'),
-            ('NJ', 'NJ'),
-            ('NM', 'NM'),
-            ('NV', 'NV'),
-            ('NY', 'NY'),
-            ('OH', 'OH'),
-            ('OK', 'OK'),
-            ('OR', 'OR'),
-            ('PA', 'PA'),
-            ('RI', 'RI'),
-            ('SC', 'SC'),
-            ('SD', 'SD'),
-            ('TN', 'TN'),
-            ('TX', 'TX'),
-            ('UT', 'UT'),
-            ('VA', 'VA'),
-            ('VT', 'VT'),
-            ('WA', 'WA'),
-            ('WI', 'WI'),
-            ('WV', 'WV'),
-            ('WY', 'WY'),
-        ],
+        choices=choices_list('addr_state_choices.csv'),
         default='CA',
     )
     submit = SubmitField("Predict")
