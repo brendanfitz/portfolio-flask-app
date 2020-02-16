8ffb0b07149f0bc52bbdf93c7799238ea11b96d6
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-02-16 13:10:31 -0500

    Update house_word_cloud.js with gryffindor colors

--- metis_app/static/js/house_word_cloud.js
+++ metis_app/static/js/house_word_cloud.js
@@ -1,7 +1,7 @@
 d3.json('/static/js/data/house_words.json', function(data) {
-  words = data['gryffindor']
+  words = data['gryffindor'];
 
-  var frequency_list = []
+  var frequency_list = [];
 
   var fontSizeMin = 12,
       fontSizeMax = 48
@@ -14,17 +14,13 @@ d3.json('/static/js/data/house_words.json', function(data) {
       size: fontSizeGenerator(fontSizeMin, fontSizeMax)
     });
   })
-  console.log(frequency_list);
-  var color = d3.scale.linear()
-          .domain([0,1,2,3,4,5,6,10,15,20,100])
-          .range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]);
 
   d3.layout.cloud().size([800, 300])
-          .words(frequency_list)
-          .rotate(0)
-          .fontSize(function(d) { return d.size; })
-          .on("end", draw)
-          .start();
+    .words(frequency_list)
+    .rotate(0)
+    .fontSize(function(d) { return d.size; })
+    .on("end", draw)
+    .start();
 
   function draw(words) {
       d3.select("#chart-area").append("svg")
@@ -39,7 +35,7 @@ d3.json('/static/js/data/house_words.json', function(data) {
           .data(words)
         .enter().append("text")
           .style("font-size", function(d) { return d.size + "px"; })
-          .style("fill", function(d, i) { return color(i); })
+          .style("fill", "#740001")
           .attr("transform", function(d) {
               return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
           })
