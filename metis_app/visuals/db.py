88338c6f891be2c1db4afe146a3edb9e088dfbba
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-02-27 09:42:06 -0500

    Update nhl_results to pull from template

--- metis_app/visuals/db.py
+++ metis_app/visuals/db.py
@@ -2,4 +2,5 @@ blog_db = {
     'genre_chart': 'genre_chart.html',
     'chloropleth': 'lending_club_chloropleth.html',
     'house_word_cloud': 'house_word_cloud.html',
+    'nhl_results': 'nhl_results.html',
 }
