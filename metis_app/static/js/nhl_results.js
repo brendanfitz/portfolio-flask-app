5e96c98a5f93476ee988ef827296af45e7ddee72
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-02-28 09:13:49 -0500

    Update nhl_results to pull game data

--- metis_app/static/js/nhl_results.js
+++ metis_app/static/js/nhl_results.js
@@ -16,34 +16,35 @@ var g = d3.select("#chart-area")
         .attr("transform", "translate(" + margin.left +
             ", " + margin.top + ")");
 
-var time = 0;
+var time = 1;
 var interval;
 var formattedData;
 
 // Tooltip
 var tip = d3.tip().attr('class', 'd3-tip')
     .html(function(d) {
-        var text = "<strong>Country:</strong> <span style='color:red'>" + d.country + "</span><br>";
-        text += "<strong>Continent:</strong> <span style='color:red;text-transform:capitalize'>" + d.continent + "</span><br>";
-        text += "<strong>Life Expectancy:</strong> <span style='color:red'>" + d3.format(".2f")(d.life_exp) + "</span><br>";
-        text += "<strong>GDP Per Capita:</strong> <span style='color:red'>" + d3.format("$,.0f")(d.income) + "</span><br>";
-        text += "<strong>Population:</strong> <span style='color:red'>" + d3.format(",.0f")(d.population) + "</span><br>";
+        var text = "<strong>Team:</strong> <span style='color:red;text-transform:capitalize'>" + d.team + "</span><br>";
+        text = "<strong>Game Number:</strong> <span style='color:red'>" + d.game_number + "</span><br>";
+        text += "<strong>Points:</strong> <span style='color:red'>" + d.points + "</span><br>";
         return text;
     });
 g.call(tip);
 
 // Scales
-var x = d3.scaleLog()
-    .base(10)
+var x = d3.scaleLinear()
     .range([0, width])
-    .domain([142, 150000]);
+    .domain([0, 84]);
 var y = d3.scaleLinear()
     .range([height, 0])
-    .domain([0, 90]);
-var area = d3.scaleLinear()
-    .range([25*Math.PI, 1500*Math.PI])
-    .domain([2000, 1400000000]);
-var continentColor = d3.scaleOrdinal(d3.schemePastel1);
+    .domain([0, 100]);
+
+d3.csv("/static/js/data/nhl_team_colors.csv").then(function(data){
+  teamColors = {};
+  data.forEach((item, i) => {
+    console.log(teamColors);
+    teamColors[item['team']] = item['color'];
+  });
+})
 
 // Labels
 var xLabel = g.append("text")
@@ -51,26 +52,25 @@ var xLabel = g.append("text")
     .attr("x", width / 2)
     .attr("font-size", "20px")
     .attr("text-anchor", "middle")
-    .text("GDP Per Capita ($)");
+    .text("Game Number");
 var yLabel = g.append("text")
     .attr("transform", "rotate(-90)")
     .attr("y", -40)
     .attr("x", -170)
     .attr("font-size", "20px")
     .attr("text-anchor", "middle")
-    .text("Life Expectancy (Years)")
+    .text("Points")
 var timeLabel = g.append("text")
     .attr("y", height -10)
     .attr("x", width - 40)
     .attr("font-size", "40px")
     .attr("opacity", "0.4")
     .attr("text-anchor", "middle")
-    .text("1800");
+    .text("1");
 
 // X Axis
 var xAxisCall = d3.axisBottom(x)
-    .tickValues([400, 4000, 40000])
-    .tickFormat(d3.format("$"));
+    .tickFormat(function(d){ return +d; });
 g.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0," + height +")")
@@ -82,42 +82,38 @@ var yAxisCall = d3.axisLeft(y)
 g.append("g")
     .attr("class", "y axis")
     .call(yAxisCall);
-
-var continents = ["europe", "asia", "americas", "africa"];
-
-var legend = g.append("g")
-    .attr("transform", "translate(" + (width - 10) +
-        "," + (height - 125) + ")");
-
-continents.forEach(function(continent, i){
-    var legendRow = legend.append("g")
-        .attr("transform", "translate(0, " + (i * 20) + ")");
-
-    legendRow.append("rect")
-        .attr("width", 10)
-        .attr("height", 10)
-        .attr("fill", continentColor(continent));
-
-    legendRow.append("text")
-        .attr("x", -10)
-        .attr("y", 10)
-        .attr("text-anchor", "end")
-        .style("text-transform", "capitalize")
-        .text(continent);
-});
+// var continents = ["europe", "asia", "americas", "africa"];
+//
+// var legend = g.append("g")
+//     .attr("transform", "translate(" + (width - 10) +
+//         "," + (height - 125) + ")");
+//
+// continents.forEach(function(continent, i){
+//     var legendRow = legend.append("g")
+//         .attr("transform", "translate(0, " + (i * 20) + ")");
+//
+//     legendRow.append("rect")
+//         .attr("width", 10)
+//         .attr("height", 10)
+//         .attr("fill", continentColor(continent));
+//
+//     legendRow.append("text")
+//         .attr("x", -10)
+//         .attr("y", 10)
+//         .attr("text-anchor", "end")
+//         .style("text-transform", "capitalize")
+//         .text(continent);
+// });
 
 d3.json("/static/js/data/nhl_results.json").then(function(data){
-    console.log(data);
-
     // Clean data
-    formattedData = data.map(function(year){
-        return year["countries"].filter(function(country){
-            var dataExists = (country.income && country.life_exp);
-            return dataExists
-        }).map(function(country){
-            country.income = +country.income;
-            country.life_exp = +country.life_exp;
-            return country;
+    formattedData = data.map(function(game_number){
+        return game_number["teams"].filter(function(team){
+            return team.points;
+        }).map(function(team){
+            team.game_number = +team.game_number;
+            team.points = +team.points;
+            return team;
         })
     });
 
@@ -131,7 +127,7 @@ $("#play-button")
         var button = $(this);
         if (button.text() == "Play"){
             button.text("Pause");
-            interval = setInterval(step, 100);
+            interval = setInterval(step, 250);
         }
         else {
             button.text("Play");
@@ -141,28 +137,28 @@ $("#play-button")
 
 $("#reset-button")
     .on("click", function(){
-        time = 0;
+        time = 1;
         update(formattedData[0]);
     })
 
-$("#continent-select")
+$("#team-select")
     .on("change", function(){
         update(formattedData[time]);
     })
 
-$("#date-slider").slider({
-    max: 2014,
-    min: 1800,
+$("#game-slider").slider({
+    max: 82,
+    min: 1,
     step: 1,
     slide: function(event, ui){
-        time = ui.value - 1800;
+        time = ui.value;
         update(formattedData[time]);
     }
 })
 
 function step(){
     // At the end of our data, loop back
-    time = (time < 214) ? time+1 : 0
+    time = (time < 82) ? time+1 : 1;
     update(formattedData[time]);
 }
 
@@ -171,18 +167,18 @@ function update(data) {
     var t = d3.transition()
         .duration(100);
 
-    var continent = $("#continent-select").val();
+    var team = $("#team-select").val();
 
     var data = data.filter(function(d){
-        if (continent == "all") { return true; }
+        if (team == "all") { return true; }
         else {
-            return d.continent == continent;
+            return d.team == team;
         }
     })
 
     // JOIN new data with old elements.
     var circles = g.selectAll("circle").data(data, function(d){
-        return d.country;
+        return d.team;
     });
 
     // EXIT old elements not present in new data.
@@ -194,18 +190,19 @@ function update(data) {
     circles.enter()
         .append("circle")
         .attr("class", "enter")
-        .attr("fill", function(d) { return continentColor(d.continent); })
+        .attr("fill", function(d) { return teamColors[d.team]; })
         .on("mouseover", tip.show)
         .on("mouseout", tip.hide)
         .merge(circles)
         .transition(t)
-            .attr("cy", function(d){ return y(d.life_exp); })
-            .attr("cx", function(d){ return x(d.income) })
-            .attr("r", function(d){ return Math.sqrt(area(d.population) / Math.PI) });
+            .attr("cy", function(d){ return y(d.points); })
+            .attr("cx", function(d){ return x(d.game_number) })
+            .attr("r", 8)
+            .attr("opacity", 0.3);
 
     // Update the time label
-    timeLabel.text(+(time + 1800))
-    $("#year")[0].innerHTML = +(time + 1800)
+    timeLabel.text(+(time))
+    $("#game_number")[0].innerHTML = +(time)
 
-    $("#date-slider").slider("value", +(time + 1800))
+    $("#game-slider").slider("value", +(time))
 }
