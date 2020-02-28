74b3fa256391726723830d5d97444c2e3cc140c2
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-02-28 09:14:09 -0500

    Temporarily remove legend from nhl results

--- metis_app/static/js/nhl_results.js
+++ metis_app/static/js/nhl_results.js
@@ -82,28 +82,6 @@ var yAxisCall = d3.axisLeft(y)
 g.append("g")
     .attr("class", "y axis")
     .call(yAxisCall);
-// var continents = ["europe", "asia", "americas", "africa"];
-//
-// var legend = g.append("g")
-//     .attr("transform", "translate(" + (width - 10) +
-//         "," + (height - 125) + ")");
-//
-// continents.forEach(function(continent, i){
-//     var legendRow = legend.append("g")
-//         .attr("transform", "translate(0, " + (i * 20) + ")");
-//
-//     legendRow.append("rect")
-//         .attr("width", 10)
-//         .attr("height", 10)
-//         .attr("fill", continentColor(continent));
-//
-//     legendRow.append("text")
-//         .attr("x", -10)
-//         .attr("y", 10)
-//         .attr("text-anchor", "end")
-//         .style("text-transform", "capitalize")
-//         .text(continent);
-// });
 
 d3.json("/static/js/data/nhl_results.json").then(function(data){
     // Clean data
