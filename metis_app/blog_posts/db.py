976dcd584d7ed56cf194d1225a9e4d1678112e55
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-03-14 13:56:58 -0400

    Add date posted to blogs_db

--- metis_app/blog_posts/db.py
+++ metis_app/blog_posts/db.py
@@ -1,41 +1,54 @@
 blog_db = {
     'api_sourcing': {
         'title': 'Sourcing Data From an API',
+        'date_posted': '2018-11-08',
     },
     'web_scraping_with_beautiful_soup': {
         'title': 'Web Scraping - Beautiful Soup',
+        'date_posted': '2019-05-02',
     },
     'web_scraping_with_selenium': {
         'title': 'Web Scraping - Selenium',
+        'date_posted': '2019-07-17',
     },
     'nlp_classification_model': {
         'title': 'Natural Language Processing (NLP) Classification Model',
+        'date_posted': '2019-12-07',
     },
     'movie_data_eda': {
         'title': 'Box Office Mojo Top 3 EDA Fun Facts Through Boxplots',
+        'date_posted': '2018-06-12',
     },
     'redshift': {
         'title': 'Redshift SQL Helper',
+        'date_posted': '2020-02-11',
     },
     'python_to_excel': {
         'title': 'Opening Excel from Python using the Subprocess Module',
+        'date_posted': '2019-08-14',
     },
     'forward_selection': {
         'title': 'Forward Selection Regression Model for Predicting Movie ROI',
+        'date_posted': '2019-03-21',
     },
     'hyperparameter_tuning_with_gridsearch': {
         'title': 'Hyperparameter Tuning with Grid Search',
+        'date_posted': '2019-11-29',
     },
     'extract_pos_with_textblob': {
         'title': 'Extracting Parts-of-Speech with TextBlob',
+        'date_posted': '2020-01-16',
     },
     'workout-log': {
         'title': 'Tracking My Workouts in Excel',
+        'date_posted': '2020-03-04',
     },
     'bash-netflix-command': {
         'title': 'Creating a Bash Command for Netflix',
+        'date_posted': '2019-04-16',
     },
     'parsing-xml': {
         'title': 'Parsing XML to Visual the Yield Curve',
+        'date_posted': '2020-03-14',
     },
 }
--- metis_app/blog_posts/views.py
+++ metis_app/blog_posts/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, Blueprint
 from metis_app.blog_posts.db import blog_db
+from datetime import datetime
 
 kwargs = dict(
     template_folder='templates/blog_posts',
@@ -11,5 +12,8 @@ blog_posts = Blueprint('blog_posts', __name__, **kwargs)
 def blog(name):
     blog_data = blog_db[name]
     title = blog_data['title']
+    date = (datetime.strptime(blog_data['date_posted'], '%Y-%m-%d')
+        .strftime('%B %d, %Y')
+    )
     template = '{}.html'.format(name)
-    return render_template(template, title=title, blog=True)
+    return render_template(template, title=title, blog=True, date=date)
