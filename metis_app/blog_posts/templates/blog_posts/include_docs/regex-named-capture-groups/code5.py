e31302b70a2fe8d40c78d9c676d4368472544dda
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-03-18 13:48:18 -0400

    Update code5.py to named capture group tags to string

--- metis_app/blog_posts/templates/blog_posts/include_docs/regex-named-capture-groups/code5.py
+++ metis_app/blog_posts/templates/blog_posts/include_docs/regex-named-capture-groups/code5.py
@@ -1,4 +1,4 @@
-pat = r'(?P<feet>\d+)′ (?P<inches>\d+\.?\d?)″'
+pat = r'(?P&lt;feet&gt;\d+)′ (?P&lt;inches&gt;\d+\.?\d?)″'
 ft_inch_columns = ft_inches_str.str.extract(pat).astype('float64')
 (df.join(ft_inch_columns)
  .head()
