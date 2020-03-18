908a9b4f6eb61733af28b4c20ea5e9548bc35683
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-03-18 15:23:43 -0400

    Add site section dropdown to navbar and blogs, models and visuals lists

--- metis_app/core/views.py
+++ metis_app/core/views.py
@@ -1,5 +1,5 @@
 # core/views.py
-from flask import render_template, request, Blueprint
+from flask import render_template, abort, request, Blueprint
 from metis_app.blog_posts.db import blog_db
 from metis_app.visuals.db import visuals_db
 
@@ -13,3 +13,12 @@ def index():
 @core.route('/about')
 def about():
     return render_template('about.html', blog=True, title="About Me, Brendan!")
+
+@core.route('/<template_name>')
+def site_section_list(template_name):
+    if template_name not in ['blogs_list', 'models_list', 'visuals_list',]:
+         abort(404)
+
+    kwargs = dict(homepage=True, blog_db=blog_db, visuals_db=visuals_db)
+    template = '{}.html'.format(template_name)
+    return render_template(template, **kwargs)
