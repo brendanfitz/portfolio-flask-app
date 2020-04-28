bb476e91a845e3a68f8cdd50b8339e7baa5794ff
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-04-28 15:35:29 -0400

    Refactor visuals database

--- metis_app/visuals/db.py
+++ metis_app/visuals/db.py
@@ -1,23 +1,30 @@
-visuals_db = {
-    'nhl_playoff_race_dashboard': {
+visuals_db = [
+    {
+        'id': 'nhl_playoff_race_dashboard',
         'title': 'NHL Playoff Race Dashboard',
     },
-    'genre_chart': {
+    {
+        'id': 'genre_chart',
         'title': 'Median ROI by Movie Genre',
     },
-    'lending_club_chloropleth': {
+    {
+        'id': 'lending_club_chloropleth',
         'title': 'Lending Club Default Rate by State',
     },
-    'house_word_cloud': {
+    {
+        'id': 'house_word_cloud',
         'title': 'Hogwarts House Word Clouds',
     },
-    'yield_curve': {
+    {
+        'id': 'yield_curve',
         'title': 'Ridin\' the Yield Curve',
     },
-    'SP500_component_stock_weightings': {
+    {
+        'id': 'SP500_component_stock_weightings',
         'title': 'S&P 500 Weighting by Constituent Stocks',
     },
-    'DowJones_component_stock_weightings': {
+    {
+        'id': 'DowJones_component_stock_weightings',
         'title': 'Dow Jones Industrial Average Weighting by Constituent Stocks',
     },
-}
+]
--- metis_app/visuals/views.py
+++ metis_app/visuals/views.py
@@ -11,10 +11,10 @@ visuals = Blueprint('visuals', __name__, **kwargs)
 
 @visuals.route('/<name>')
 def visual(name):
-    if name not in visuals_db.keys():
+    if name not in [x['id'] for x in visuals_db]:
         abort(404)
 
-    visual_data = visuals_db[name]
+    visual_data = next(filter(lambda x: name == x['id'], visuals_db))
     title = visual_data['title']
     template = '{}.html'.format(name)
     return render_template(template, title=title, viz_page=True)
