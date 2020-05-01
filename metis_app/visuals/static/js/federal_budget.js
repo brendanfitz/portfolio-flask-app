1d10c186a41099b82243c436543fa450d31b9199
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-05-01 08:44:09 -0400

    Add three pieCharts to federal budget visualization

--- metis_app/visuals/static/js/federal_budget.js
+++ metis_app/visuals/static/js/federal_budget.js
@@ -1,10 +1,16 @@
 
 
 
-var pieChart = new PieChart('#chart-area');
+var dataClean = {}
+var pieChart1 = new PieChart('#chart-area1');
+var pieChart2 = new PieChart('#chart-area2');
+var pieChart3 = new PieChart('#chart-area3');
 
 d3.csv("static/js/data/outlays_by_function_2020.csv").then(function(data) {
-  var dataClean = {}
   data.forEach(function(d) { dataClean[d.function] = +d.amount; })
-  pieChart.wrangleData(dataClean);
+  pieChart1.wrangleData(dataClean);
+  pieChart2.wrangleData(dataClean);
+  pieChart3.wrangleData(dataClean);
 })
+
+console.log(dataClean)
