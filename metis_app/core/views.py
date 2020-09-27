b1dc10bdeeb3c47281b4baba702ca767c0209923
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-09-27 17:18:40 -0400

    Update models_list page with cards instead of an unordered list

--- metis_app/core/views.py
+++ metis_app/core/views.py
@@ -2,6 +2,7 @@
 from flask import render_template, abort, request, Blueprint
 from metis_app.blog_posts.db import blog_db
 from metis_app.visuals.db import visuals_db
+from metis_app.ml_models.db import ml_db
 
 blog_db.sort(reverse=True, key=lambda x: x['date_posted'])
 
@@ -27,6 +28,11 @@ def site_section_list(template_name):
     if template_name not in ['blogs_list', 'models_list', 'visuals_list',]:
          abort(404)
 
-    kwargs = dict(homepage=True, blog_db=blog_db, visuals_db=visuals_db)
+    kwargs = dict(
+        homepage=True,
+        blog_db=blog_db,
+        visuals_db=visuals_db,
+        ml_db=ml_db,
+    )
     template = '{}.html'.format(template_name)
     return render_template(template, **kwargs)
