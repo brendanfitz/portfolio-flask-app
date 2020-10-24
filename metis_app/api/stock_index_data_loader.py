1f6fbc13871a73662cfd7aeb2938dace4ad2e67a
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2020-10-24 13:03:33 -0500

    Modify stock index api to pull from postgres ec2 database

--- metis_app/api/stock_index_data_loader.py
+++ metis_app/api/stock_index_data_loader.py
@@ -3,27 +3,25 @@ import json
 from os import environ
 import pandas as pd
 import boto3
+import psycopg2
+from metis_app.api import cc
 
-class StockIndexScraper(object):
+class StockIndexDataLoader(object):
     
     uris = ['sp500', 'dowjones']
 
     def __init__(self, stock_index_name, from_s3):
         self.stock_index_name = stock_index_name
         self.from_s3 = from_s3
-        self.data = self.scrape_index_component_stocks()
+        self.data = self.load_data()
     
-    def scrape_index_component_stocks(self):
-        if self.stock_index_name not in StockIndexScraper.uris:
-            msg = f'stock_index_name must be in {StockIndexScraper.uris}'
+    def load_data(self):
+        if self.stock_index_name not in self.uris:
+            msg = f'stock_index_name must be in {self.uris}'
             raise ValueError(msg)
         
-        df_scraped_func = self.df_scraped_from_s3 if self.from_s3 else self.df_scraped_from_web
-        df_scraped = df_scraped_func()
-    
-        industries = self.scrape_stock_industries()
-
-        df = self.clean_df_scraped_and_merge_industries(df_scraped, industries)
+        data_loader = self.load_from_s3 if self.from_s3 else self.load_from_db
+        df = data_loader()
     
         data = (df
             .reset_index()
@@ -31,15 +29,22 @@ class StockIndexScraper(object):
         )
         return data
     
-    def df_scraped_from_web(self):
-        url = r'https://www.slickcharts.com/{}'.format(self.stock_index_name)
-        response = requests.get(url)
-        df = (pd.read_html(response.text)[0]
+    def load_from_db(self):
+        conn = psycopg2.connect(**cc)
+        
+        sql = ("SELECT * FROM visuals.index_component_stocks "
+               f"WHERE stock_index_name= '{self.stock_index_name}'")
+
+        df = (pd.read_sql(sql, conn)
+            .rename(columns=lambda x: x.replace('_', ' ').title())
             .set_index('Symbol')
         )
+
+        conn.close()
+
         return df
     
-    def df_scraped_from_s3(self):
+    def load_from_s3(self):
         aws_config = {
             'aws_access_key_id': environ.get('METIS_APP_AWS_ACCESS_KEY_ID'),
             'aws_secret_access_key': environ.get('METIS_APP_AWS_SECRET_KEY'),
@@ -56,50 +61,3 @@ class StockIndexScraper(object):
         )
     
         return df
-
-    def scrape_stock_industries(self):
-        if self.stock_index_name == 'sp500':
-            url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
-            table_num = 0
-        elif self.stock_index_name == 'dowjones':
-            url = 'https://en.wikipedia.org/wiki/Dow_Jones_Industrial_Average'
-            table_num = 1
-        else:
-            raise ValueError('Index Must Be "sp500" or "dowjones"')
-        df = (pd.read_html(url)[table_num]
-            .assign(Symbol=lambda x: x.Symbol.str.replace('NYSE:\xa0', ''))
-            .set_index('Symbol')
-            .rename(columns={'GICS Sector': 'Industry'})
-            .loc[:, ['Industry']]
-        )
-        return df
-    
-
-    @staticmethod
-    def all_stocks_have_industries(df):
-        mask = df.loc[:, 'Industry'].isna()
-        return df.loc[mask, ].empty
-    
-    @staticmethod
-    def clean_df_scraped_and_merge_industries(df_scraped, industries):
-        columns = ['Company', 'Weight', 'Price']
-        df_without_weights = (df_scraped
-            .loc[:, columns]
-            .join(industries)
-            .sort_values(['Industry', 'Weight'], ascending=[True, False])
-        )
-    
-        industry_weights = (df_without_weights
-            .groupby(['Industry'], as_index=False)['Weight'].sum()
-            .set_index('Industry')
-            .rename(columns={'Weight': 'Industry Weight'})
-        )
-    
-        df = (df_without_weights
-            .join(industry_weights, on=['Industry'])
-            .fillna("Unknown")
-            .sort_values(['Industry Weight', 'Weight'], ascending=[False, False])
-        )
-        
-        return df
-    
