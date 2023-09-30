030ad220ecc72ebc6176cbdded515bc4366067b0
brendanfitz <brendanjfitzpatrick0@gmail.com>
2023-09-30 13:28:45 -0500

    Refactor core views with date posted

--- portfolio/core/views.py
+++ portfolio/core/views.py
@@ -9,8 +9,8 @@ def index():
     kwargs = dict(
         homepage=True,
         blog_db=list(dbs.blogs.find({}, {'id': 1, 'title': 1}).sort("date_posted", -1).limit(6)),
-        visuals_db=list(dbs.visuals.find({}, {'id': 1, 'title': 1}).limit(6)),
-        ml_db=list(dbs.ml_models.find({}, {'id': 1, 'title': 1}).limit(6)),
+        visuals_db=list(dbs.visuals.find({}, {'id': 1, 'title': 1}).sort("date_posted", -1).limit(6)),
+        ml_db=list(dbs.ml_models.find({}, {'id': 1, 'title': 1}).sort("date_posted", -1).limit(6)),
         title="Homepage",
     )
     return render_template('index.html', **kwargs)
@@ -24,11 +24,16 @@ def site_section_list(template_name):
     if template_name not in ['blogs_list', 'models_list', 'visuals_list',]:
          abort(404)
 
+    if template_name == 'blogs_list':
+        c = dbs.blogs
+    elif template_name == 'models_list':
+        c = dbs.ml_models
+    else:
+        c = dbs.visuals
+
     kwargs = dict(
         homepage=True,
-        blog_db=list(dbs.blogs.find({}, {'id': 1, 'title': 1}).sort("date_posted", -1)),
-        visuals_db=list(dbs.visuals.find({}, {'id': 1, 'title': 1})),
-        ml_db=list(dbs.ml_models.find({}, {'id': 1, 'title': 1})),
+        posts=list(c.find({}, {'id': 1, 'title': 1}).sort("date_posted", -1)),
         title=template_name.replace('_', ' ').title()
     )
     template = '{}.html'.format(template_name)
